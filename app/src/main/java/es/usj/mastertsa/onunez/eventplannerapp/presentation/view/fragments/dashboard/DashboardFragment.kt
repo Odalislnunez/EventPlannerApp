cc6c7b2b58b64157ee1f963f0a144f4a7727f160
Odalis Lidisset Nuñez Espinal <onunez@farmatrix.com.do>
2022-07-26 10:44:47 -0400

    Design Changes.

--- app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/presentation/view/fragments/dashboard/DashboardFragment.kt
+++ app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/presentation/view/fragments/dashboard/DashboardFragment.kt
@@ -1,20 +1,18 @@
 package es.usj.mastertsa.onunez.eventplannerapp.presentation.view.fragments.dashboard
 
+import androidx.lifecycle.ViewModelProvider
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
-import androidx.fragment.app.Fragment
-import androidx.lifecycle.ViewModelProvider
 import es.usj.mastertsa.onunez.eventplannerapp.databinding.FragmentDashboardBinding
 
 class DashboardFragment : Fragment() {
 
     private var _binding: FragmentDashboardBinding? = null
 
-    // This property is only valid between onCreateView and
-    // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(
@@ -39,4 +37,5 @@ class DashboardFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
 }
