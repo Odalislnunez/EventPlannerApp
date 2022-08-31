d26cd3db5285069db393b327280ebd14d923f898
Odalis Lidisset Nuñez Espinal <onunez@farmatrix.com.do>
2022-08-31 12:09:05 -0400

    Changes in Invitation model.

--- app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/domain/models/Invitation.kt
+++ app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/domain/models/Invitation.kt
@@ -1,8 +1,13 @@
 package es.usj.mastertsa.onunez.eventplannerapp.domain.models
 
+import android.os.Parcelable
+import es.usj.mastertsa.onunez.eventplannerapp.utils.Constants.INFO_NOT_SET
+import kotlinx.android.parcel.Parcelize
+
+@Parcelize
 data class Invitation(
-    val invitationId: Int,
-    val event: Event,
-    val user: User,
-    val answer: String
-)
+    val invitationId: Int = 0,
+    val eventId: String = INFO_NOT_SET,
+    val userId: String = INFO_NOT_SET,
+    val answer: String = INFO_NOT_SET
+): Parcelable
