9a67d9e265c8f49ec3f250432c94b86a086c5bc3
Odalis <odalislnunez@hotmail.com>
2022-09-08 00:10:54 -0400

    Changes.

--- app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/data/firebase/implementations/EventRepository.kt
+++ app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/data/firebase/implementations/EventRepository.kt
@@ -1,5 +1,7 @@
 package es.usj.mastertsa.onunez.eventplannerapp.data.firebase.implementations
 
+import android.content.ContentValues.TAG
+import android.util.Log
 import com.google.firebase.firestore.CollectionReference
 import com.google.firebase.firestore.SetOptions
 import es.usj.mastertsa.onunez.eventplannerapp.di.FirebaseModule
@@ -80,10 +82,22 @@ class EventRepository @Inject constructor (
     override suspend fun getAllPublicEvents(): Flow<DataState<List<Event>>> = flow {
         emit(DataState.Loading)
         try {
-            val publicEvents = eventsCollection//.whereEqualTo("type", "Public")
+            var publicEvents: List<Event> = mutableListOf()
+            eventsCollection.whereEqualTo("type", "Public")
                 .get()
-                .await()
-                .toObjects(Event::class.java)
+                .addOnSuccessListener { documents ->
+                    for (document in documents) {
+                        Log.d(TAG, "${document.id} => ${document.data}")
+                    }
+                }
+                .addOnFailureListener { exception ->
+                    Log.w(TAG, "Error getting documents: ", exception)
+                }
+
+//            val publicEvents = eventsCollection.whereEqualTo("type", "Public")
+//                .get()
+//                .await()
+//                .toObjects(Event::class.java)
             emit(DataState.Success(publicEvents))
             emit(DataState.Finished)
         }catch (e: Exception){
