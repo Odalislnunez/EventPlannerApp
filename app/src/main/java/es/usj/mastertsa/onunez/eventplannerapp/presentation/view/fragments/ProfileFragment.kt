0a59c72177b9e6626161693be2b6a69daf8e6f82
Odalis <odalislnunez@hotmail.com>
2022-09-08 13:46:50 -0400

    Changes.

--- app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/presentation/view/fragments/ProfileFragment.kt
+++ app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/presentation/view/fragments/ProfileFragment.kt
@@ -85,7 +85,23 @@ class ProfileFragment : Fragment() {
             binding.ivProfilePicture.isEnabled = false
         }
         else {
-//            viewModel.getUserInObjectData(USER_LOGGED_IN_ID)
+            viewModel.getUserInObjectData(USER_LOGGED_IN_ID)
+
+            Log.d(TAG, "User Fragment: " + mUser.name)
+            activity?.showToast("User: " + mUser.name)
+
+            if (mUser.name != INFO_NOT_SET) {
+                binding.tvEmail.text = mUser.email
+                binding.etName.setText(mUser.name)
+                binding.etLastname.setText(mUser.lastname)
+                binding.etPhone.setText(contact.phoneNumber)
+                mProfileImageURL = contact.profileImage
+                isImageSelected = true
+                comesFromExtras = true
+                binding.ivProfilePicture.load(contact.profileImage)
+            }
+
+            viewModel.getUserInObjectData(USER_LOGGED_IN_ID)
 
             Log.d(TAG, "User Fragment: " + mUser.name)
             activity?.showToast("User: " + mUser.name)
