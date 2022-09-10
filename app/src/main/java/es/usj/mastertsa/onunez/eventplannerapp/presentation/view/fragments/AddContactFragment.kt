10c2fc86a2bf6bbf446f9efce620c5ce1461050f
Odalis <odalislnunez@hotmail.com>
2022-09-10 16:49:42 -0400

    Contacts Changes.

--- app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/presentation/view/fragments/AddContactFragment.kt
+++ app/src/main/java/es/usj/mastertsa/onunez/eventplannerapp/presentation/view/fragments/AddContactFragment.kt
@@ -5,11 +5,12 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.DialogFragment
 import androidx.fragment.app.viewModels
 import es.usj.mastertsa.onunez.eventplannerapp.databinding.FragmentAddContactBinding
 import es.usj.mastertsa.onunez.eventplannerapp.presentation.viewmodel.ContactsViewModel
 
-class AddContactFragment : Fragment() {
+class AddContactFragment : DialogFragment() {
     private var _binding: FragmentAddContactBinding? = null
     private val binding get() = _binding!!
 
